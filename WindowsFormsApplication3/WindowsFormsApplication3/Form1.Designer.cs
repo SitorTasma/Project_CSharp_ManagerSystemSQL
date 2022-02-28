a9806708343efa73d8efe9c9a8d48a11b404c6b7
SitorTasma <100101949+SitorTasma@users.noreply.github.com>
2022-02-28 08:45:34 +0700

    CC

--- WindowsFormsApplication3/WindowsFormsApplication3/Form1.Designer.cs
+++ WindowsFormsApplication3/WindowsFormsApplication3/Form1.Designer.cs
@@ -37,9 +37,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(22, 29);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 26);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.Size = new System.Drawing.Size(80, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Mã Dự Án";
             // 
@@ -49,7 +50,7 @@
             this.listDA.FormattingEnabled = true;
             this.listDA.Location = new System.Drawing.Point(111, 26);
             this.listDA.Name = "listDA";
-            this.listDA.Size = new System.Drawing.Size(121, 21);
+            this.listDA.Size = new System.Drawing.Size(270, 21);
             this.listDA.TabIndex = 1;
             this.listDA.SelectedIndexChanged += new System.EventHandler(this.listDA_SelectedIndexChanged);
             // 
@@ -58,7 +59,7 @@
             this.outData_gridv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.outData_gridv.Location = new System.Drawing.Point(25, 77);
             this.outData_gridv.Name = "outData_gridv";
-            this.outData_gridv.Size = new System.Drawing.Size(240, 150);
+            this.outData_gridv.Size = new System.Drawing.Size(356, 150);
             this.outData_gridv.TabIndex = 2;
             this.outData_gridv.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.outData_gridv_CellContentClick);
             // 
@@ -66,7 +67,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(410, 252);
             this.Controls.Add(this.outData_gridv);
             this.Controls.Add(this.listDA);
             this.Controls.Add(this.label1);
---
+++ WindowsFormsApplication3/WindowsFormsApplication3/Form2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form2 : Form
+    {
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void controlbar_BN_RefreshItems(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
